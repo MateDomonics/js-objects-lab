0801cbf4d23f7071cb9cc1c616bde86f02f9369a
MateDomonics <20093156@mail.wit.ie>
2022-09-16 14:55:34 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,14 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : "3 Walkers Lane",
+    previous_owners: [ {
+      name: "Pat Smith",
+      address: "1 Main Street",
+    }, {
+      name: "Sheila Dwyer",
+      address: "2 High Street"
+    }
+    ],
     type : {
       make: "Toyota",
       model: "Corolla",
@@ -22,4 +31,5 @@ const aCar = {
   }
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
   console.log(aCar.owner + "' car registration number is " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number);
-  console.log("It is a red car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.texture + " interior.")
+  console.log("It is a red car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.texture + " interior.");
+  console.log("First owner : " + aCar.previous_owners[0].name);
