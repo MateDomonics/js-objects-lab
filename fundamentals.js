60d055e0d5b7fe9f9dea06acbeba57028fbc12ca
MateDomonics <20093156@mail.wit.ie>
2022-09-16 14:44:55 +0100

    Dynamic Properties

--- fundamentals.js
+++ fundamentals.js
@@ -12,5 +12,14 @@ const aCar = {
   }
   };
   
+  aCar.mileage = 10000;
+  aCar.colour = {
+    exterior: "red",
+    interior: {
+      texture: "Leather",
+      shade: "cream"
+    }
+  }
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
   console.log(aCar.owner + "' car registration number is " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number);
+  console.log("It is a red car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.texture + " interior.")
