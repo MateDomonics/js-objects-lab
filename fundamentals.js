95a6c0adde439ce50aff7817492ff6195c15ea2f
MateDomonics <20093156@mail.wit.ie>
2022-09-16 15:15:07 +0100

    Loop Constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -14,6 +14,7 @@ const aCar = {
       model: "Corolla",
       CC: 1.8
     },
+    features: ["Parking assist", "Alarm", "Tow-bar"],
     registration : { 
       year: 201,
       countyCode: "WD",
@@ -29,7 +30,21 @@ const aCar = {
       shade: "cream"
     }
   }
+
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
   console.log(aCar.owner + "' car registration number is " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number);
   console.log("It is a red car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.texture + " interior.");
   console.log("First owner : " + aCar.previous_owners[0].name);
+
+  
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]);
+  }
+
+  for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name);
+  }
+
+  for (let p in aCar.type) {
+    console.log(p.toUpperCase() + " = " + aCar.type[p]);
+  }
